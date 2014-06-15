0bb5d8048319acae93b5d3c5b883932ca482b710
seb-kro <sebastian.krott@t-online.de>
2014-06-16 00:20:59 +0200

    Cover Test, Extended Modularity Test (renewed, some files were not committed)

---
+++ LAS2peer Sample Service/src/test/i5/las2peer/services/servicePackage/graph/CoverTest.java
@@ -0,0 +1,101 @@
+package i5.las2peer.services.servicePackage.graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import i5.las2peer.services.servicePackage.testsUtil.TestGraphFactory;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.la4j.matrix.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
+
+import y.base.Node;
+
+public class CoverTest {
+
+	private Cover cover;
+	
+	/*
+	 * Creates a cover for the sawmill graph
+	 */
+	@Before 
+	public void sawmillCoverSetUp() {
+		CustomGraph graph = TestGraphFactory.getSawmillGraph();
+		Matrix memberships = new Basic2DMatrix(graph.nodeCount(), 5);
+		for(int i=0; i<memberships.rows(); i++) {
+			for(int j=0; j<memberships.columns(); j++) {
+				memberships.set(i, j, i*j);
+			}
+		}
+		cover = new Cover(graph, memberships);
+	}
+	
+	/*
+	 * Tests cover normalization.
+	 */
+	@Test
+	public void testDoNormalize() {
+		cover.doNormalize();
+		Matrix memberships = cover.getMemberships();
+		// First row must remain unchanged and sum up to zero precisely.
+		double rowSum = 0;
+		for(int j=0; j<memberships.columns(); j++) {
+			rowSum += memberships.get(0, j);
+		}
+		// Other rows must sum up to one.
+		assertEquals(rowSum, 0.0, 0.0);
+		for(int i=1; i<memberships.rows(); i++) {
+			rowSum = 0;
+			for(int j=0; j<memberships.columns(); j++) {
+				rowSum += memberships.get(i, j);
+			}
+			assertEquals(rowSum, 1.0, 0.00001);
+		}
+	}
+	
+	/*
+	 * Tests return values of the belonging factors.
+	 */
+	@Test
+	public void testGetBelongingFactor() {
+		CustomGraph graph = cover.getGraph();
+		Node[] nodes = graph.getNodeArray();
+		for(int i=0; i<graph.nodeCount(); i++) {
+			for(int j=0; j<cover.communityCount(); j++) {
+				assertEquals(cover.getBelongingFactor(nodes[i], j), i*j, 0.0);
+			}
+		}
+	}
+	
+	/*
+	 * Tests the return value of the community count.
+	 */
+	@Test
+	public void testCommunityCount() {
+		assertEquals(cover.communityCount(), 5);
+	}
+	
+	/*
+	 * Tests the return value of the community indices of a node.
+	 */
+	@Test
+	public void testGetCommunityIndices() {
+		CustomGraph graph = cover.getGraph();
+		Node[] nodes = graph.getNodeArray();
+		List<Integer> indicesA = cover.getCommunityIndices(0);
+		List<Integer> indicesB = cover.getCommunityIndices(nodes[0]);
+		assertTrue(indicesA.isEmpty());
+		assertTrue(indicesB.isEmpty());
+		indicesA = cover.getCommunityIndices(1);
+		indicesB = cover.getCommunityIndices(nodes[1]);
+		assertTrue(indicesA.size() == 4);
+		assertTrue(indicesB.size() == 4);
+		for(int i=1; i<= 4; i++) {
+			assertTrue(indicesA.contains(i));
+			assertTrue(indicesB.contains(i));
+		}
+	}
+
+}
---
+++ LAS2peer Sample Service/src/test/i5/las2peer/services/servicePackage/metrics/ExtendedModularityMetricTest.java
@@ -0,0 +1,50 @@
+package i5.las2peer.services.servicePackage.metrics;
+
+import i5.las2peer.services.servicePackage.algorithms.AlgorithmFactory;
+import i5.las2peer.services.servicePackage.algorithms.OverlappingCommunityDetectionAlgorithm;
+import i5.las2peer.services.servicePackage.algorithms.SpeakerListenerLabelPropagationHelpers.ListenerRuleCommand;
+import i5.las2peer.services.servicePackage.algorithms.SpeakerListenerLabelPropagationHelpers.PopularityListenerRule;
+import i5.las2peer.services.servicePackage.algorithms.SpeakerListenerLabelPropagationHelpers.SpeakerRuleCommand;
+import i5.las2peer.services.servicePackage.algorithms.SpeakerListenerLabelPropagationHelpers.UniformSpeakerRule;
+import i5.las2peer.services.servicePackage.graph.Cover;
+import i5.las2peer.services.servicePackage.graph.CustomGraph;
+import i5.las2peer.services.servicePackage.testsUtil.TestGraphFactory;
+
+import org.junit.Test;
+import org.la4j.matrix.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
+
+public class ExtendedModularityMetricTest {
+
+	/*
+	 * Assures that modularity is 0 if only one community exists.
+	 */
+	@Test
+	public void testExtendedModularityWithOneCommunity() {
+		CustomGraph graph = TestGraphFactory.getSawmillGraph();
+		Matrix memberships = new Basic2DMatrix(graph.nodeCount(), 1);
+		for(int i=0; i<memberships.rows(); i++) {
+			memberships.set(i, 0, 1);
+		}
+		Cover cover = new Cover(graph, memberships);
+		ExtendedModularityMetric metric = new ExtendedModularityMetric();
+		double index = metric.getMetricIndex(cover);
+		System.out.println("1 Community");
+		System.out.println(index);
+	}
+	
+	@Test
+	public void testExtendedModularityOnSawmillSLPA() {
+		CustomGraph graph = TestGraphFactory.getSawmillGraph();
+		AlgorithmFactory factory = AlgorithmFactory.getAlgorithmFactory();
+		SpeakerRuleCommand speakerRule = new UniformSpeakerRule();
+		ListenerRuleCommand listenerRule = new PopularityListenerRule();
+		OverlappingCommunityDetectionAlgorithm algo = factory.getSpeakerListenerLabelPropagationAlgorithm(100, 0.04, speakerRule, listenerRule);
+		Cover cover = algo.detectOverlappingCommunities(graph);
+		ExtendedModularityMetric metric = new ExtendedModularityMetric();
+		double index = metric.getMetricIndex(cover);
+		System.out.println("Sawmill SLPA");
+		System.out.println(index);
+	}
+
+}
