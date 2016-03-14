78278466df837f2cc5bafd384806218a3b72a6f3
Shahriar Robbani <mineme.shahriar@gmail.com>
2016-03-14 02:08:51 +0100

    Start Script will created by ant...

--- bin/start_network.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this script starts a las2peer node providing the example service of this project
-# pls execute it from the root folder of your deployment, e. g. ./bin/start_network.sh
-
-java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 uploadStartupDirectory\(\'etc/startup\'\) startService\(\'i5.las2peer.services.ocd.ServiceClass@1.0\',\'ocdPass\'\) startWebConnector interactive
