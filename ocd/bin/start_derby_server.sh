9f274b69deb86072269b37d9aec758bd32e165d1
Christoph <christoph.kraemer@rwth-aachen.de>
2017-07-02 22:07:27 +0200

    remove derby
    The service already uses HSQLDB as database. This commit removes the old
    Derby remains.

--- ocd/bin/start_derby_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this script starts a derby server configured for this service
-# please execute it from the applications /ocd/bin directory
-
-cd ../derby
-java -cp "../../lib/*" org.apache.derby.drda.NetworkServerControl start -h 127.0.0.1 -p 1527
--- ocd/bin/start_ij.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this script starts a derby database monitoring tool called ij
-# please execute it from the applications /ocd/bin directory
-
-cd ../derby
-java -cp "../../lib/*" org.apache.derby.tools.ij
