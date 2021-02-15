c3a6f07a609fc1c924e5625e1018292dd25f320f
MarleneID <73598490+MarleneID@users.noreply.github.com>
2021-02-15 21:17:22 +0100

    Added junit test for maximal cliques
    Added junit text

---
+++ src/test/i5/las2peer/services/ocd/algorithms/ClusteringUtils/MaximalCliqueGraphRepresentationTest.java
@@ -0,0 +1,32 @@
+package i5.las2peer.services.ocd.algorithms.ClusteringUtils;
+
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.util.HashSet;
+import org.junit.Ignore;
+import org.junit.Test;
+import y.base.Node;
+import i5.las2peer.services.ocd.adapters.AdapterException;
+import i5.las2peer.services.ocd.algorithms.utils.OcdAlgorithmException;
+import i5.las2peer.services.ocd.algorithms.utils.MaximalCliqueGraphRepresentation;
+import i5.las2peer.services.ocd.graphs.CustomGraph;
+import i5.las2peer.services.ocd.testsUtils.OcdTestGraphFactory;
+
+public class MaximalCliqueGraphRepresentationTest {
+	
+
+	@Test
+	public void testOnMaximalCliqueGraph() throws OcdAlgorithmException, InterruptedException, AdapterException, FileNotFoundException, IllegalArgumentException, ParseException {
+		CustomGraph graph = OcdTestGraphFactory.getMaximalCliqueGraph();
+		MaximalCliqueGraphRepresentation MCl = new MaximalCliqueGraphRepresentation();
+		HashSet<HashSet<Node>> maxClq = MCl.cliques(graph);
+			
+		for(HashSet<Node> hs : maxClq){
+			System.out.print("Clique");
+			for(Node node : hs){
+				System.out.print(node);
+			}
+			System.out.println("End");
+       }
+	}
+}
