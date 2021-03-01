f169e18cf611c8865ee51ab345142aa1083b9efb
Marlene <73598490+MarleneID@users.noreply.github.com>
2021-03-01 13:06:45 +0100

    minor test changes

--- src/test/i5/las2peer/services/ocd/algorithms/AntColonyOptimizationTest.java
+++ src/test/i5/las2peer/services/ocd/algorithms/AntColonyOptimizationTest.java
@@ -9,12 +9,10 @@ import java.util.HashMap;
 
 import org.junit.Test;
 import org.la4j.matrix.Matrix;
-import org.la4j.matrix.dense.Basic2DMatrix;
 import org.la4j.vector.Vector;
 import org.la4j.vector.dense.BasicVector;
 
 import i5.las2peer.services.ocd.adapters.AdapterException;
-import i5.las2peer.services.ocd.algorithms.AntColonyOptimizationAlgorithm;
 import i5.las2peer.services.ocd.algorithms.utils.Ant;
 import i5.las2peer.services.ocd.algorithms.utils.MaximalCliqueGraphRepresentation;
 import i5.las2peer.services.ocd.algorithms.utils.OcdAlgorithmException;
@@ -69,67 +67,37 @@ public class AntColonyOptimizationTest {
 	}
 	
 	@Test
+	public void testACO() throws OcdAlgorithmException, InterruptedException, AdapterException, FileNotFoundException, IllegalArgumentException, ParseException {
+		CustomGraph graph = OcdTestGraphFactory.getDolphinsGraph() ;
+		AntColonyOptimizationAlgorithm ACO = new AntColonyOptimizationAlgorithm();
+		Cover c = ACO.detectOverlappingCommunities(graph);
+		System.out.print(c);
+	}
+	
+	@Test
 	public void testDecoding() throws OcdAlgorithmException, InterruptedException, AdapterException, FileNotFoundException, IllegalArgumentException, ParseException {
 		CustomGraph graph = OcdTestGraphFactory.getMaximalCliqueGraph() ;
 		AntColonyOptimizationAlgorithm ACO = new AntColonyOptimizationAlgorithm();
 		CustomGraph MCR = ACO.representationScheme(graph);
-		
-		Vector cover = new BasicVector();
+		Vector cover = new BasicVector(5);
 		cover.set(0, 0);
 		cover.set(1, 0);
 		cover.set(2, 1);
 		cover.set(3, 1);
-		cover.set(3, 1);
-		ACO.decodeMaximalCliques(MCR, cover);
+		cover.set(4, 1);
+		ACO.decodeMaximalCliques(graph, cover);
 	}
-	
-
-	
-	
-	
 	
 	@Test
 	public void testNegativeRatioAssociation() throws OcdAlgorithmException, InterruptedException, AdapterException, FileNotFoundException, IllegalArgumentException, ParseException {
 		CustomGraph graph = OcdTestGraphFactory.getMaximalCliqueGraph();
-		//Cover cover = new Cover(graph);
 		
-		/*Matrix memberships = new Basic2DMatrix(9,5); 
-		memberships.set(0, 0, 1);
-		memberships.set(1, 0, 1);
-		memberships.set(8, 0, 1);
-		memberships.set(1, 1, 1);
-		memberships.set(2, 1, 1);
-		memberships.set(8, 1, 1);
-		memberships.set(2, 2, 1);
-		memberships.set(3, 2, 1);
-		memberships.set(7, 2, 1);
-		memberships.set(3, 3, 1);
-		memberships.set(5, 3, 1);
-		memberships.set(6, 3, 1);
-		memberships.set(7, 3, 1);
-		memberships.set(3, 4, 1);
-		memberships.set(4, 4, 1);
-		memberships.set(5, 4, 1);
-		*/
-		//cover.setMemberships(memberships);
-		
-		Vector cover = new BasicVector();
+		Vector cover = new BasicVector(5);
 		cover.set(0, 0);
 		cover.set(1, 0);
-		cover.set(8, 0);
-		cover.set(1, 1);
 		cover.set(2, 1);
-		cover.set(8, 1);
-		cover.set(2, 2);
-		cover.set(3, 2);
-		cover.set(7, 2);
-		cover.set(3, 3);
-		cover.set(5, 3);
-		cover.set(6, 3);
-		cover.set(7, 3);
-		cover.set(3, 4);
-		cover.set(4, 4);
-		cover.set(5, 4);
+		cover.set(3, 1);
+		cover.set(4, 1);
 		
 		AntColonyOptimizationAlgorithm ACO = new AntColonyOptimizationAlgorithm();
 		double NRC = ACO.negativeRatioAssociation(graph, cover);
@@ -141,45 +109,12 @@ public class AntColonyOptimizationTest {
 	@Test
 	public void testCutRatio() throws OcdAlgorithmException, InterruptedException, AdapterException, FileNotFoundException, IllegalArgumentException, ParseException {
 		CustomGraph graph = OcdTestGraphFactory.getMaximalCliqueGraph();
-		/*Cover cover = new Cover(graph);
-		Matrix memberships = new Basic2DMatrix(9,5); 
-		memberships.set(0, 0, 1);
-		memberships.set(1, 0, 1);
-		memberships.set(8, 0, 1);
-		memberships.set(1, 1, 1);
-		memberships.set(2, 1, 1);
-		memberships.set(8, 1, 1);
-		memberships.set(2, 2, 1);
-		memberships.set(3, 2, 1);
-		memberships.set(7, 2, 1);
-		memberships.set(3, 3, 1);
-		memberships.set(5, 3, 1);
-		memberships.set(6, 3, 1);
-		memberships.set(7, 3, 1);
-		memberships.set(3, 4, 1);
-		memberships.set(4, 4, 1);
-		memberships.set(5, 4, 1);
-		
-		cover.setMemberships(memberships);
-		*/
-		
-		Vector cover = new BasicVector();
+		Vector cover = new BasicVector(5);
 		cover.set(0, 0);
 		cover.set(1, 0);
-		cover.set(8, 0);
-		cover.set(1, 1);
 		cover.set(2, 1);
-		cover.set(8, 1);
-		cover.set(2, 2);
-		cover.set(3, 2);
-		cover.set(7, 2);
-		cover.set(3, 3);
-		cover.set(5, 3);
-		cover.set(6, 3);
-		cover.set(7, 3);
-		cover.set(3, 4);
-		cover.set(4, 4);
-		cover.set(5, 4);
+		cover.set(3, 1);
+		cover.set(4, 1);
 		AntColonyOptimizationAlgorithm ACO = new AntColonyOptimizationAlgorithm();
 		double CR = ACO.cutRatio(graph, cover);
 		System.out.println("Cut Ratio");
