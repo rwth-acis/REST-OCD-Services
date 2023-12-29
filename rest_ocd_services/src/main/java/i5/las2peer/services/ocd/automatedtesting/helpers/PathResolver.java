50f2cc40277911a1ef50b5dee2d4d77eb3db4e5b
beka-zhvania <beka.zhvania@rwth-aachen.de>
2023-12-29 12:34:38 +0100

    add partially completed test class generation used for assistant GPT API communication

---
+++ rest_ocd_services/src/main/java/i5/las2peer/services/ocd/automatedtesting/helpers/PathResolver.java
@@ -0,0 +1,59 @@
+package i5.las2peer.services.ocd.automatedtesting.helpers;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathResolver {
+
+    /**
+     * Finds the root directory of the project by traversing up from the current directory.
+     * The root is identified by a specific marker, such as the presence of a '.git' directory.
+     * This method is useful for determining the project root both in a standalone and a Gradle context.
+     *
+     * @return The absolute path to the root directory of the project as a String.
+     *         Returns an empty string if the root directory cannot be determined.
+     */
+    private static String findRootDirectory() {
+        Path current = Paths.get("").toAbsolutePath();
+        while (current != null && !isRootDirectory(current)) {
+            current = current.getParent();
+        }
+        return current != null ? current.toString() : "";
+    }
+
+    /**
+     * Checks if the given path is the root directory of the project.
+     * The root directory is identified by the presence of a specific marker, such as a '.git' directory.
+     *
+     * @param path The path to check.
+     * @return true if the path contains the marker indicating it is the root directory, false otherwise.
+     */
+    private static boolean isRootDirectory(Path path) {
+        // Check for a unique marker in the root directory
+        // For example, looking for a '.git' directory or a specific file
+        File marker = path.resolve(".git").toFile();
+        return marker.exists();
+    }
+
+    /**
+     * Resolves a relative path to an absolute path based on the root directory of the project.
+     * The root directory is dynamically determined based on the presence of a specific marker,
+     * such as a '.git' directory. This method is useful for generating consistent file paths
+     * regardless of whether the code is executed via Gradle or standalone.
+     *
+     * @param relativePath The relative path to be resolved.
+     * @return The absolute path as a String, formed by appending the relative path to the project's root directory.
+     */
+    public static String resolvePath(String relativePath) {
+        String rootDirectory = findRootDirectory();
+        return Paths.get(rootDirectory, relativePath).toString();
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        String filePath = resolvePath("gpt/classfiles/GeneratedSskAlgorithmTest.java");
+        System.out.println(filePath);
+        // Now use this filePath to write your file
+    }
+}
--- rest_ocd_services/src/test/java/i5/las2peer/services/ocd/ocdatestautomation/OCDATestClassGenerator.java
+++ rest_ocd_services/src/test/java/i5/las2peer/services/ocd/ocdatestautomation/OCDATestClassGenerator.java
@@ -1,10 +1,15 @@
 package i5.las2peer.services.ocd.ocdatestautomation;
 
+import i5.las2peer.services.ocd.automatedtesting.PromptGenerator;
 import i5.las2peer.services.ocd.automatedtesting.helpers.FileHelpers;
+import i5.las2peer.services.ocd.automatedtesting.helpers.PathResolver;
 import i5.las2peer.services.ocd.automatedtesting.ocdparser.OCDAParser;
+import i5.las2peer.services.ocd.graphs.CustomGraph;
 import i5.las2peer.services.ocd.ocdatestautomation.test_interfaces.*;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 import static i5.las2peer.services.ocd.automatedtesting.OCDATestAutomationConstants.*;
@@ -126,11 +131,13 @@ public class OCDATestClassGenerator {
                 "public class " +  ocdaTestClassName +"Test implements "
                 + baseInterfaceNamesToImplement + " {\n" +
                 "\n" +
-                "\tOcdAlgorithm algo;\n" +
+                "\t" + ocdaName + " algo; // Variable to access " + ocdaName +"\n" +
+                "\t" + CustomGraph.class.getSimpleName() + " defaultCustomGraphToUse; // Variable to access CustomGraph used in tests (unless test defines a different graph) \n" +
                 "\n" +
                 "\t@BeforeEach\n" +
                 "\tpublic void setup() {\n" +
                 "\t\talgo = new "+ ocdaName +"();\n" +
+                "\t\tdefaultCustomGraphToUse = new " + CustomGraph.class.getSimpleName() + "();\n" +
                 "\t}\n" +
                 "\n" +
                 "\t@Override\n" +
@@ -139,26 +146,180 @@ public class OCDATestClassGenerator {
                 "\t}\n" +
                 "\n" +
                 "\n" +
-                "\t// ChatGPT Code \n" +
-                "\n" +
                 generatePartiallyCompletedGraphTypeTests(ocdaCode)+ "\n" +
                 "}\n"
         );
 
+
         // Write test class to a file
-        generateAndWriteFile("gpt","classfiles/"+ocdaTestClassName+"Test.java", stringBuilder.toString());
+        generateAndWriteFile(PathResolver.resolvePath("gpt/classfiles/"+ocdaTestClassName+"Test.java"), stringBuilder.toString(),false);
 
 
     }
+
+
+    /**
+     * Generates and writes a test class for specified methods of an OCD algorithm. This method can create both
+     * a test class that should be completed (e.g., by GPT) and a reference class. The reference class is used to
+     * compare whether the auto-completion altered unintended parts of the code.
+     *
+     * @param ocdaCode The file containing the OCD algorithm code.
+     * @param methodsToTest A list of method names from the OCD algorithm that should be tested.
+     * @param isReferenceClass A boolean flag to indicate if the generated class is a reference test class.
+     *                         If true, generates a reference class; otherwise, generates a class for completion.
+     */
+    public static void generateAndWriteOCDAMethodsTestClass(File ocdaCode, List<String> methodsToTest, boolean isReferenceClass){
+
+        // Extract OCD algorithm name from the class file
+        String ocdaName = OCDAParser.getClassName(ocdaCode);
+
+        // Differentiate between a test class that should be completed (by GPT) and a reference test class which is
+        // used to compare if the changes made by GPT make sense (e.g. was something deleted that wasn't supposed to?)
+        String ocdaTestClassName = ocdaName;;
+        if (!isReferenceClass) {
+            ocdaTestClassName = "Generated" + ocdaName;
+        }
+
+
+        // Initialize an empty StringBuilder to concatenate strings
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        // Merge import statements from the OCD algorithm class and its test class, since the algorithm class
+        // imports might be needed for the test class if the user wants to test OCD algorithm methods.
+        List<String> mergedImports = mergeAndSortLists(extractSortedImports(ocdaCode),
+                generateTestClassImports(ocdaCode));
+
+
+        /* Generate partially completed test class */
+
+        // package depends on where the auto-generated class should be placed.
+        String testClassPackage = TEST_CLASS_PACKAGE_STRING;
+        if (!isReferenceClass) {
+            testClassPackage = AUTO_GENERATED_TEST_CLASS_PACKAGE;
+        }
+
+        // Append package to the test class
+        stringBuilder.append("package " + testClassPackage + ";").append("\n\n");
+
+        // Append import statements to the test class
+        mergedImports.forEach(importStatement -> stringBuilder.append(importStatement).append("\n"));
+
+        // Generate unit partially completed unit tests for each method that should be tested
+        StringBuilder partiallyCompletedOCDAMethodTests = new StringBuilder();
+        if (methodsToTest != null) {
+            methodsToTest.forEach(methodToTest -> {
+
+                // If the specified method exists in the specified OCD class, then create a partially completed unit test
+                Boolean ocdaMethodExists = !OCDAParser.getMethodSignature(ocdaCode, methodToTest).equals("");
+                if (ocdaMethodExists) {
+                    partiallyCompletedOCDAMethodTests.append(PromptGenerator.generateOCDAMethodUnitTestString(methodToTest)).append("\n\n");
+                }
+
+            });
+        }
+
+
+        // Add partially completed unit tests to the test class
+        stringBuilder.append("\n\n" +
+                "public class " +  ocdaTestClassName +"Test {\n" +
+                "\n" +
+                "\t" + ocdaName + " algo; // Variable to access " + ocdaName +"\n" +
+                "\t" + CustomGraph.class.getSimpleName() + " defaultCustomGraphToUse; // Variable to access CustomGraph used in tests (unless test defines a different graph) \n" +
+                "\n" +
+                "\t@BeforeEach\n" +
+                "\tpublic void setup() {\n" +
+                "\t\talgo = new "+ ocdaName +"();\n" +
+                "\t\tdefaultCustomGraphToUse = new " + CustomGraph.class.getSimpleName() + "();\n" +
+                "\t}\n" +
+                "\n" +
+                partiallyCompletedOCDAMethodTests.toString()+ "\n" +
+                "}\n"
+        );
+
+        // Write test class to a file
+        generateAndWriteFile(PathResolver.resolvePath("gpt/classfiles/"+ocdaTestClassName+"Test.java"), stringBuilder.toString(),false);
+
+
+    }
+
+
+
+    public void initializeGraphTypeTestRelatedFiles(String ocdaName){
+        String contentRoot = System.getProperty("user.dir");
+        String relativePath = "/src/main/java/i5/las2peer/services/ocd/algorithms/";
+        String ocdaCodePath =  contentRoot + File.separator + relativePath + ocdaName + ".java";
+        File ocdaCode = new File(ocdaCodePath);
+        // generate a test class with partially completed unit tests to which the GPT-generated tests  should be compared to
+        generateAndWriteOCDTestClass(ocdaCode, true);
+        // Generate OCD algorithm test class with partially completed unit tests that will act as a reference for comparison
+        generateAndWriteOCDTestClass(ocdaCode, false);
+    }
+
+    public void initializeOCDAMethodTestFiles(String ocdaName, List<String> methodNames){
+        String contentRoot = System.getProperty("user.dir");
+        String relativePath = "/src/main/java/i5/las2peer/services/ocd/algorithms/";
+        String ocdaCodePath =  contentRoot + File.separator + relativePath + ocdaName + ".java";
+        File ocdaCode = new File(ocdaCodePath);
+
+        /* Generate and write test classes that hold partially completed unit tests related to specific OCDA methods */
+        //methodNames = Arrays.asList("Item1", "calculateTransitiveLinkWeight", "calculateMemberships");
+        // generate a test class with partially completed unit tests to which the GPT-generated tests  should be compared to
+        generateAndWriteOCDAMethodsTestClass(ocdaCode, methodNames,true);
+        // Generate OCD algorithm test class with partially completed unit tests  that will act as a reference for comparison
+        generateAndWriteOCDAMethodsTestClass(ocdaCode, methodNames, false);
+    }
+
+
 
 
     public static void main(String[] args) {
-        File ocdaCode = new File(FileHelpers.getOCDAPath("SskAlgorithm.java"));
-
-        // generate a test class with partially completed unit tests to which the GPT-generated tests  should be compared to
-        generateAndWriteOCDTestClass(ocdaCode, true);
-
-        // Generate OCD algorithm test class with partially completed unit tests
-        generateAndWriteOCDTestClass(ocdaCode, false);
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "initializeGraphTypeTestRelatedFiles":
+                    if (args.length > 1) {
+                        String ocdaName = args[1];
+                        new OCDATestClassGenerator().initializeGraphTypeTestRelatedFiles(ocdaName);
+                    } else {
+                        System.out.println("OCDA name not provided for Graph Type Tests.");
+                    }
+                    break;
+                case "initializeOCDAMethodTestFiles":
+                    if (args.length > 2) {
+                        String ocdaName = args[1];
+                        List<String> methodNames = Arrays.asList(args).subList(2, args.length);
+                        new OCDATestClassGenerator().initializeOCDAMethodTestFiles(ocdaName, methodNames);
+                    } else {
+                        System.out.println("OCDA name or method names not provided for OCDA Method Tests.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid arguments.");
+                    break;
+            }
+        } else {
+            System.out.println("No arguments provided.");
+        }
     }
+
+
+
+
+//    public static void main(String[] args) {
+//        File ocdaCode = new File(FileHelpers.getOCDAPath("SskAlgorithm.java"));
+//
+//        /* Generate and write test classes that hold partially completed unit tests related to graph types */
+//
+//        // generate a test class with partially completed unit tests to which the GPT-generated tests  should be compared to
+////        generateAndWriteOCDTestClass(ocdaCode, true);
+////        // Generate OCD algorithm test class with partially completed unit tests that will act as a reference for comparison
+////        generateAndWriteOCDTestClass(ocdaCode, false);
+//
+////        /* Generate and write test classes that hold partially completed unit tests related to specific OCDA methods */
+////        List<String> stringList = Arrays.asList("Item1", "calculateTransitiveLinkWeight", "calculateMemberships");
+////        // generate a test class with partially completed unit tests to which the GPT-generated tests  should be compared to
+////        generateAndWriteOCDAMethodsTestClass(ocdaCode, stringList,true);
+////        // Generate OCD algorithm test class with partially completed unit tests  that will act as a reference for comparison
+////        generateAndWriteOCDAMethodsTestClass(ocdaCode, stringList, false);
+//    }
 }
